5d48d98f08345980df8e9235fe59cd2c354f30b5
brahmarajesh27 <67675940+brahmarajesh27@users.noreply.github.com>
2020-07-02 17:40:05 +0530

    Add files via upload

---
+++ python_queen.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  2 00:15:31 2020
+
+@author: 717680
+"""
+
+
+from itertools import permutations
+N=3
+sol=1
+COL= range(N)
+for itr in permutations(COL):
+    if N==len(set(itr[i]+i for i in COL))==len(set(itr[i]-i for i in COL)):
+        print('Solution' + str(itr) +'\n')
+        print("\n".join('o'*i +'Q'+'o'*N(-i-1) for i in itr)
+           
+             
+        
+                                                
+                                              
+                                              
+    
+   
+                                             
+                                             
