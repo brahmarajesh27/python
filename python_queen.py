c80512953c92720e32bbee5e59f12555a467171f
brahmarajesh27 <67675940+brahmarajesh27@users.noreply.github.com>
2020-07-04 21:20:54 +0530

    Update python_queen.py

--- python_queen.py
+++ python_queen.py
@@ -4,17 +4,17 @@ Created on Thu Jul  2 00:15:31 2020
 
 @author: 717680
 """
-
-
 from itertools import permutations
-N=3
+N=8
+
 sol=1
 COL= range(N)
 for itr in permutations(COL):
     if N==len(set(itr[i]+i for i in COL))==len(set(itr[i]-i for i in COL)):
-        print('Solution' + str(itr) +'\n')
-        print("\n".join('o'*i +'Q'+'o'*N(-i-1) for i in itr)
-           
+        print('Solution' +str(sol)+ str(itr) +'\n')
+        print("\n".join('o'*i +'Q'+'o'* (N-i-1) for i in itr)+"\n\n\n\n")
+        break
+
              
         
                                                 
